840ad5ff4e1ae4428e6fe7b5db47f94292a4ce12
NIKITHA BAIJU <nairnisha346@gmail.com>
2026-06-18 08:52:54 +0530

    Implement navbar shadow on scroll
    Add scroll event listener to change navbar style

---
+++ app.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll", () => {
+
+const navbar = document.querySelector(".navbar");
+
+if(window.scrollY > 50){
+navbar.style.boxShadow =
+"0 5px 15px rgba(0,0,0,0.4)";
+}else{
+navbar.style.boxShadow = "none";
+}
+
+});
+
+console.log("Portfolio Loaded Successfully");
